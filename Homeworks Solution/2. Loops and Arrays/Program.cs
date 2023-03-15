7480e6d5f9a7fd4cec70fecd72a06cd75128d903
Jankovv295 <jankov295@gmail.com>
2023-03-15 17:11:47 +0100

    Homework C#

---
+++ Homeworks Solution/2. Loops and Arrays/Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//TASK 1:
+//Create new console application “SwapNumbers”. Input 2 numbers from the console and then swap the values of the variables
+//so that the first variable has the second value and the second variable the first value.
+
+Console.WriteLine("Enter two numbers");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int temp;
+
+temp = number1;
+number1 = number2;
+number2 = temp;
+
+Console.WriteLine($"After swapping number1 has value {number1} and number2 has value {number2}");
+
+
+
+
+
+//TASK 2:
+//Make a console application called SumOfEven. Inside it create an array of 6 integers. 
+//Get numbers from the input, find and print the sum of the even numbers from the array:
+
+
+int[] arr = new int[6];
+int sum = 0;
+
+Console.WriteLine("Enter 6 numbers");
+for (int i = 0; i < 6; i++)
+{
+    int arrInput = Convert.ToInt32(Console.ReadLine());
+    arr[i] = arrInput;
+
+    if (arr[i] % 2 == 0)
+    {
+        sum += arr[i];
+    }
+}
+Console.WriteLine($"The sum of the numbers you entered that are divided by 2 is {sum}");
+
+
+
+
+
+//TASK 3:
+//Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names (strings).
+//Get a number from the console between 1 and 2 and print the students from that group in the console.
+
+string[] studentsg1 = { "marko", "marija", "martin", "biljana", "kristijan" };
+string[] studentsg2 = { "viktor", "nenad", "nikola", "monika", "boban" };
+
+Console.WriteLine("enter student group: ( there are 1 and 2 )");
+string response = Console.ReadLine(); //namerno e string da isprobam:)
+
+
+if (response == "1")
+{
+    foreach (var item in studentsg1)
+    {
+        Console.WriteLine(item);
+
+    }
+}
+else if (response == "2")
+{
+    foreach (var item in studentsg2)
+    {
+        Console.WriteLine(item);
+
+    }
+}
+
+
+//istoto so int
+
+//int response = Convert.ToInt32(Console.ReadLine());
+
+//if (response == 1)
+//{
+//    foreach (var item in studentsG1)
+//    {
+//        Console.WriteLine(item);
+
+//    }
+//}
+//else if (response == 2)
+//{
+//    foreach (var item in studentsG2)
+//    {
+//        Console.WriteLine(item);
+
+//    }
+//}
+
+
+
+
+
+
+
+
---
+++ Homeworks Solution/3. Methods/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+//TASK 1
+//Create 4 methods called Sum, Subtract, Multiply and Divde. The method will accept two numbers as parameter
+//and return a result from the operation of the two numbers. Ask a person from the console to enter operators
+//Then ask the person to enter two numbers. Use switch ** Additional: Check divide by zero **
+
+int Sum (int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int Subtract (int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int Multiply (int num1, int num2)
+{
+    return (num1 * num2);
+}
+
+int Divide (int num1, int num2)
+{
+    //int sum = num1 / num2;
+    //if (num2 == 0)
+    //{
+    //    Console.WriteLine("ERROR");
+    //}
+    return num1 / num2;
+}
+
+string Calculator (char operation, int num1, int num2)
+{
+
+    int result = 0;
+    string consoleResult = null;
+
+    switch (operation)
+    {
+        case '+':
+           result = Sum(num1, num2);
+            consoleResult = $"The result of summing the two numbers you entered is: {result}";
+            break;
+        case '-':
+            result = Subtract(num1, num2);
+            if (num2 == 0) consoleResult = "ERROR: You can't divide with 0";
+            consoleResult = $"The result of subtracting the two numbers you entered is: {result}";
+            break;
+        case '*':
+            result = Multiply(num1, num2);
+            consoleResult = $"The result of multiplying the two numbers you entered is: {result}";
+            break;
+        case '/':
+            result = Divide(num1, num2);
+            consoleResult = $"The result of dividing the two numbers you entered is: {result}";
+            break;
+        default:
+            consoleResult = "Invalid operator input";
+            break;
+    }
+    return consoleResult;
+}
+
+Console.WriteLine("Enter the operation you want to be executed +, -, *, /");
+char inputOperation = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Enter two numbers");
+int inputNum1 = Convert.ToInt32(Console.ReadLine());
+int inputNum2 = Convert.ToInt32(Console.ReadLine());
+
+string variableForCalculator = Calculator(inputOperation, inputNum1, inputNum2);
+Console.WriteLine(variableForCalculator);
+
+
+
