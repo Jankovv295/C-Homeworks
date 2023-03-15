4e54f97fa0f3282e3d616da1f9a1ca6ab6807508
Jankovv295 <jankov295@gmail.com>
2023-03-15 17:16:43 +0100

    Homework C#

--- Homeworks Solution/3. Methods/Program.cs
+++ Homeworks Solution/3. Methods/Program.cs
@@ -21,11 +21,6 @@ int Multiply (int num1, int num2)
 
 int Divide (int num1, int num2)
 {
-    //int sum = num1 / num2;
-    //if (num2 == 0)
-    //{
-    //    Console.WriteLine("ERROR");
-    //}
     return num1 / num2;
 }
 
